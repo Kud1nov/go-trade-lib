75564c625a77baa36e323e21c0f2744e82b8a2b1
Kudinov <a.kudinov@city-mobil.ru>
2021-07-31 16:09:41 +0300

    add mutex to stat trades

--- statistics/trades.go
+++ statistics/trades.go
@@ -1,5 +1,7 @@
 package statistics
 
+import "sync"
+
 type Trades interface {
 	TradesCount() int
 	OrdersCount() int
@@ -17,6 +19,7 @@ type Trades interface {
 }
 
 type trades struct {
+	mx            sync.RWMutex
 	trades        int
 	orders        int
 	longTrades    int
@@ -26,50 +29,74 @@ type trades struct {
 }
 
 func (t *trades) TradesCount() int {
+	t.mx.RLock()
+	defer t.mx.RUnlock()
 	return t.trades
 }
 
 func (t *trades) OrdersCount() int {
+	t.mx.RLock()
+	defer t.mx.RUnlock()
 	return t.orders
 }
 
 func (t *trades) LongTrades() int {
+	t.mx.RLock()
+	defer t.mx.RUnlock()
 	return t.longTrades
 }
 
 func (t *trades) ShortTrades() int {
+	t.mx.RLock()
+	defer t.mx.RUnlock()
 	return t.shortTrades
 }
 
 func (t *trades) WinningTrades() int {
+	t.mx.RLock()
+	defer t.mx.RUnlock()
 	return t.winningTrades
 }
 
 func (t *trades) LosingTrades() int {
+	t.mx.RLock()
+	defer t.mx.RUnlock()
 	return t.losingTrades
 }
 
 func (t *trades) UpdateTrades(v int) {
+	t.mx.RLock()
+	defer t.mx.RUnlock()
 	t.trades = v
 }
 
 func (t *trades) UpdateOrders(v int) {
+	t.mx.RLock()
+	defer t.mx.RUnlock()
 	t.orders = v
 }
 
 func (t *trades) UpdateLongTrades(v int) {
+	t.mx.RLock()
+	defer t.mx.RUnlock()
 	t.longTrades = v
 }
 
 func (t *trades) UpdateShortTrades(v int) {
+	t.mx.RLock()
+	defer t.mx.RUnlock()
 	t.shortTrades = v
 }
 
 func (t *trades) UpdateWinningTrades(v int) {
+	t.mx.RLock()
+	defer t.mx.RUnlock()
 	t.winningTrades = v
 }
 
 func (t *trades) UpdateLosingTrades(v int) {
+	t.mx.RLock()
+	defer t.mx.RUnlock()
 	t.losingTrades = v
 }
 
